30bdbcb99e6bc3f6ad9e4bdcee0abda54b60fcbe
renansantos <renansantosmendes@gmail.com>
2019-05-20 21:16:33 -0300

    implementação do MOEAD - finalização da implementação da primeira parte do algoritmo: inicialização dos pesos e dos vizinhos - finalização da implementação da inicialização do ponto z* (ponto de referência)
    (PONTO DE ATENÇÃO: atributo numberOfAggregatedObjectives deve ser inicializado corretamente - olhar no código onde 
    deve ser feito -> evitar futura confusão)

---
+++ NetBeansProjects/PhD_MOEAD/src/main/java/Algorithms/MOEAD.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Algorithms;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.uma.jmetal.algorithm.multiobjective.moead.util.MOEADUtils;
+import org.uma.jmetal.operator.CrossoverOperator;
+import org.uma.jmetal.operator.MutationOperator;
+import org.uma.jmetal.operator.impl.crossover.DifferentialEvolutionCrossover;
+import org.uma.jmetal.problem.Problem;
+import org.uma.jmetal.solution.DoubleSolution;
+
+/**
+ *
+ * @author renansantos
+ */
+@SuppressWarnings("serial")
+public class MOEAD extends AbstractMOEAD<DoubleSolution>{
+    protected DifferentialEvolutionCrossover differentialEvolutionCrossover ;
+
+  public MOEAD(Problem<DoubleSolution> problem,
+      int populationSize,
+      int resultPopulationSize,
+      int maxEvaluations,
+      MutationOperator<DoubleSolution> mutation,
+      CrossoverOperator<DoubleSolution> crossover,
+      FunctionType functionType,
+      String dataDirectory,
+      double neighborhoodSelectionProbability,
+      int maximumNumberOfReplacedSolutions,
+      int neighborSize) {
+    super(problem, populationSize, resultPopulationSize, maxEvaluations, crossover, mutation, functionType,
+        dataDirectory, neighborhoodSelectionProbability, maximumNumberOfReplacedSolutions,
+        neighborSize);
+
+    differentialEvolutionCrossover = (DifferentialEvolutionCrossover)crossoverOperator ;
+  }
+
+  @Override public void run() {
+    initializePopulation() ;
+    initializeUniformWeight();
+    initializeNeighborhood();
+    initializeIdealPoint() ;
+
+    evaluations = populationSize ;
+    do {
+      int[] permutation = new int[populationSize];
+      MOEADUtils.randomPermutation(permutation, populationSize);
+
+      for (int i = 0; i < populationSize; i++) {
+        int subProblemId = permutation[i];
+
+        NeighborType neighborType = chooseNeighborType() ;
+        List<DoubleSolution> parents = parentSelection(subProblemId, neighborType) ;
+
+        differentialEvolutionCrossover.setCurrentSolution(population.get(subProblemId));
+        List<DoubleSolution> children = differentialEvolutionCrossover.execute(parents);
+
+        DoubleSolution child = children.get(0) ;
+        mutationOperator.execute(child);
+        problem.evaluate(child);
+
+        evaluations++;
+
+        updateIdealPoint(child);
+        updateNeighborhood(child, subProblemId, neighborType);
+      }
+    } while (evaluations < maxEvaluations);
+
+  }
+
+  protected void initializePopulation() {
+    population = new ArrayList<>(populationSize);
+    for (int i = 0; i < populationSize; i++) {
+      DoubleSolution newSolution = (DoubleSolution)problem.createSolution();
+
+      problem.evaluate(newSolution);
+      population.add(newSolution);
+    }
+  }
+
+  @Override public String getName() {
+    return "MOEAD" ;
+  }
+
+  @Override public String getDescription() {
+    return "Multi-Objective Evolutionary Algorithm based on Decomposition" ;
+  }
+}
